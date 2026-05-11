02c24de627e4884f4914dffdc0f196bf8e2ca5cc
tyagisuruchi59 <suruchityagi972@gmail.com>
2026-05-11 23:56:05 +0530

    push

--- AlertService/program.cs
+++ AlertService/program.cs
@@ -15,7 +15,7 @@ using AlertService.Services;
 using Quartz;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.WebHost.UseUrls("http://0.0.0.0:80");
+builder.WebHost.UseUrls("http://0.0.0.0:8080");
 
 // DATABASE
 builder.Services.AddDbContext<AppDbContext>(options =>
